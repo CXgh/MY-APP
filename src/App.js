9b82a90bb83a38c0f11ce3e54da7f5a3bb35b9c7
chenxi <chenxi@shandianyun.com>
2020-04-10 10:32:37 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React,{Component} from 'react'
+
+const About = function(){
+  return(
+    <div>1</div>
+  )
+}
+const Inbox = function(){
+  return(
+    <div>2</div>
+  )
+}
+const Home = function(){
+  return(
+    <div>3</div>
+  )
 }
 
-export default App;
+class App extends Component{
+  constructor(props){
+    super(props)
+    let Child;
+    switch (this.state.route) {
+      case '/about': Child = About; break;
+      case '/inbox': Child = Inbox; break;
+      default:      Child = Home;
+    }
+  }
+  getInitialState() {
+    return {
+      route: window.location.hash.substr(1)
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: window.location.hash.substr(1)
+      })
+    })
+  }
+  
+  render(){
+    let Child;
+    return (
+      <div>
+        <h1>App</h1>
+        <ul>
+          <li><a href="#/about">About</a></li>
+          <li><a href="#/inbox">Inbox</a></li>
+        </ul>
+        <Child/>
+      </div>
+    )
+  }
+      
+}
+export default App
